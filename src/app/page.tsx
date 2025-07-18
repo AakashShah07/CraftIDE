9befc2753b0cb3ef53832187863f1af5f0c6bc80
AakashShah07 <angelayusender@gmail.com>
2025-07-19 00:17:29 +0530

    Handling Some sample to test these things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button"
+import {prisma} from "@/lib/db";
 
-const Page = () => {
+const Page = async() => {
+  const users = await prisma.user.findMany();
   return (
     <div className="font-bold text-rose-500">
-      Hello world
-      <Button variant={"new"}>
-        k
-      </Button>
+      {JSON.stringify(users, null, 2)}
     </div>
   )
 }
