9dcca2678eb1fe1a7e1d49db45abfc06a9d2e52f
AakashShah07 <angelayusender@gmail.com>
2025-07-20 00:10:21 +0530

    04: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
+"use client"
 
-const Page = async () => {
-  const quearyClient = getQueryClient();
-  void quearyClient.prefetchQuery(
-    trpc.createAI.queryOptions({ text: "Aakash Prefetch" })
-  );
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import {toast} from "sonner";
+
+const Page =  () => {
+ 
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background job invoked successfully!");
+    },
+    onError: (error) => {
+      toast.error(`Failed to invoke background job: ${error.message}`);
+    },
+  }));
 
   return (
-    <HydrationBoundary state={dehydrate(quearyClient)}>
-      <Suspense fallback={<p>Loading......</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button disabled={invoke.isPending} onClick={()=> invoke.mutate({text:"John"})}>
+        Invoke Background Job
+      </Button>
+    </div>
+
   );
 };
 
