008a445334880441037b40aa217915726281fa95
AakashShah07 <angelayusender@gmail.com>
2025-07-19 19:40:15 +0530

    03-trpc setup completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-"use client";
-// 
-import { useTRPC } from "@/trpc/client"
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-const Page = () => {
-  
-  const trpc = useTRPC();
-  trpc.createAI.queryOptions({ text: "world" });
+const Page = async () => {
+  const quearyClient = getQueryClient();
+  void quearyClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "Aakash Prefetch" })
+  );
 
   return (
-    <div >
-      Hello world
-    </div>
-  )
-}
+    <HydrationBoundary state={dehydrate(quearyClient)}>
+      <Suspense fallback={<p>Loading......</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
 
-export default Page
+export default Page;
